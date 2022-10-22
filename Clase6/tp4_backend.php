8e0afa490356c00c2ae6d22c1cbbe84089cdf7b6
rosirossi <94757259+rosirossi@users.noreply.github.com>
2022-10-22 16:08:05 -0300

    Add files via upload

---
+++ Clase6/tp4_backend.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Array</title>
+  </head>
+  <body>
+    <h1>Trabajo Práctico 4: ARRAY</h1>
+    <ol>
+      <li>
+        <?php
+        $n_par= ["2","4","6","8","10","12","14","16","18","20"];
+        print "<pre>\n";
+        print_r($n_par);
+        print "</pre>\n";
+
+         ?>
+      </li>
+    </ol>
+  </body>
+</html>
